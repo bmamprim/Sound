7e8f1ca80022fd641fba198e653372695bab870d
bmamprim <bmamprim@ibm.com>
2023-11-15 18:52:06 -0300

    funcao create autor

--- dao/AutorDAO.java
+++ dao/AutorDAO.java
@@ -1,6 +1,12 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import modelo.Autor;
 
 public class AutorDAO {
     private Connection connection;
@@ -9,7 +15,22 @@ public class AutorDAO {
         this.connection = connection;
     }
     
-    public void createAutor(){};
+    public void createAutor(Autor autor){
+        try {
+            String sql = "";
+
+            try (PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+                pstm.setString(1, autor.getNome_original());
+                pstm.setString(2, autor.getCpf());
+                pstm.setString(3, autor.getNome_artistico());
+
+                pstm.execute();
+            }
+        } catch(SQLException e) {
+            throw new RuntimeException(e);
+        }
+    };
 
     public void retrieveAutor() {};
 
